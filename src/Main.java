06d302118a0ed67280117573bfc59ad1ecf60d80
Shakti Sampad Swain <shaktisampadswain@gmail.com>
2025-02-21 17:00:47 +0530

    learning oops with java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        User user1 = new User();
+        user1.set_age(21);
+        user1.set_name("shakti");
+
+
+        System.out.println(user1.get_name() + " is " + user1.get_age() + " years old");
     }
 }
---
+++ src/User.java
@@ -0,0 +1,19 @@
+public class User {
+    private String name;
+    private int age;
+
+
+    public void set_name(String name){
+        this.name = name;
+    }
+    public void set_age(int age){
+        this.age = age;
+    }
+
+    public String get_name(){
+       return name;
+    }
+    public int get_age(){
+        return age;
+    }
+}
